98fd0eef6e775af16a5d48d0122d0f801b644296
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-07-23 22:47:55 +0530

    taking user input

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        int[][] arr = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+
+        }
     }
 }
